5427b7d573e449dd2c00226c9b5175f2d744c921
vamsi <vijayvamsi550@gmail.com>
2026-05-26 22:56:30 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=AMERICA
+PERSON2=IRAN
+
+echo "$PERSON1 : Hey $PERSON2, we are going to blast you!"
+echo "$PERSON2 : yes, please you are welcomeJ"
+echo "$PERSON1 : Hey $PERSON2 I am serious"
+echo "$PERSON2 : Hey $PERSON1,we are also serious"
