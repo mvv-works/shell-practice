233903c2ce050739bbb3582a631cfa7d576573cb
vamsi <vijayvamsi550@gmail.com>
2026-05-30 12:38:08 +0530

    hello vamsi

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,11 +3,11 @@
 #TIME_STAMP=$(date)
 #echo "$TIME_STAMP"
 
-START_TIME=$(date)
+START_TIME=$(date +%s)
 
-sleep 10
+sleep 12
 
-END_TIME=$(date)
+END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($START_TIME-$END_TIME))
 echo "script execuited in $TOTAL_TIME seconds"
