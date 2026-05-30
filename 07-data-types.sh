a9b0475913510bc8ac95de2db8e1e8838bafc652
vamsi <vijayvamsi550@gmail.com>
2026-05-30 15:02:01 +0530

    hello vamsi

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -7,3 +7,10 @@ SUM=$(($NUM1+$NUM2))
 
 echo "Sum is $SUM"
 
+#arrays
+
+FRUITS=("Banana" "Apple" "pappaya")
+echo "all movies are : ${MOVIES[@]}"
+echo "first movie is : ${MOVIES[00]}"
+echo "second movie is : ${MOVIES[1]}"
+echo "third movie is : ${MOVIES[2]}"
