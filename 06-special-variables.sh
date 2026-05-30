38e08e04bc655a9d709bcd60fdd69a0f479007eb
vamsi <vijayvamsi550@gmail.com>
2026-05-30 14:18:52 +0530

    hello vamsi

---
+++ 06-special-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "All variables passed to the script : $@"
+echo "Number of variables passed : $#"
+echo "First variable : $1"
+echo "script name : $0"
+echo "who is running this : $USER"
+echo "which directory : $PWD"
