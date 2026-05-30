9d51afaa1180ce1432a7a4b363c4291e6a65162e
vamsi <vijayvamsi550@gmail.com>
2026-05-30 12:06:07 +0530

    hello vamsi

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please Enter Your User Names:"
-READ USER_NAME
+read USER_NAME
 echo "User Name is USER_NAME"
 
 echo "Please Enter Your Password:"
-READ -s PASSWD
+read -s PASSWD
 echo "Password is $PASSWD"
