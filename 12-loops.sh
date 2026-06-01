9ab320231e006bd5b6af6d18666455d3894b5fd1
vamsi <vijayvamsi550@gmail.com>
2026-06-01 12:18:47 +0530

    api doc added

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 user_id=$(id -u)
+
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"  # /home/ec2-user/shell-logs/10-logs.sh.log
 
 # check root access or not
 if [ $user_id -ne 0 ]; then
-    echo "please run this script w
-    ith root access" 
+    echo "please run this script with root access"
     exit 1
 fi
 
@@ -16,36 +16,18 @@ fi
 # 2nd arg -> exit code
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "installing $1 failed ! " | tee -a $LOGS_FILE
+        echo "installing $1 failed ! "
         exit 1
     else
-        echo "installling $1 success" | tee -a $LOGS_FILE
+        echo "installling $1 success"
     fi
 }
 
 
+for package in $@
+do 
+    echo "installing $package"
 
-#installing mysql
-
-dnf list installed mysql &>> $LOGS_FILE
-
-if [ $? -ne 0 ]; then
-    echo "installing mysql" | tee -a $LOGS_FILE
-    dnf install mysql -y  &>> $LOGS_FILE
-    VALIDATE mysql $?   # 1st arg -> MySQL ; 2nd arg -> exit is based of previosu command
-else 
-    echo "mysql is already installed...SKIPPING" | tee -a $LOGS_FILE
-fi
+done
 
 
-# installing nginx
-
-dnf list installed mysql  &>> $LOGS_FILE
-
-if [ $? -ne 0 ]; then
-    echo "installing nginx"
-    dnf install nginx -y  &>> $LOGS_FILE
-    VALIDATE nginx $?   # 1st arg -> nginx ; 2nd arg -> exit is based of previosu command
-else 
-    echo "nginx is already installed...SKIPPING" | tee -a $LOGS_FILE
-fi
