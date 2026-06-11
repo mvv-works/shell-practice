c8f9aab4602fa3c5fd7a865504acff86495a4b99
vamsi <vijayvamsi550@gmail.com>
2026-06-11 11:21:58 +0530

    hello

---
+++ 19-logs-cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DAYS=${2:-14}
+
+if [ -z $SOURCE_DIR ]; then 
+    echo "ERROR:: Missing parameters"
+    echo "USAGE:: $0 <source-dir> [days(optional default to 14)]"
+    exit 1
+fi
+
