07f8c422585f2de2e0263975afadc6089c83c552
vamsi <vijayvamsi550@gmail.com>
2026-06-11 13:11:24 +0530

    hello

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -3,18 +3,18 @@
 SOURCE_DIR=$1
 DAYS=${2:-14}
 
-if [ -z $SOURCE_DIR ]; then 
+if [ -z "$SOURCE_DIR" ]; then 
     echo "ERROR:: Missing parameters"
     echo "USAGE:: $0 <source-dir> [days(optional default to 14)]"
     exit 1
 fi
 
-if [ ! -d $SOURCE_DIR ];then
+if [ ! -d "$SOURCE_DIR" ];then
     echo "Error: Directory: $SOURCE_DIR does not exist"
     exit 1
 fi
 
-echo "scanning $SOURCE_DIR for log files older than 14 days"
+echo "scanning $SOURCE_DIR for log files older than $DAYS days"
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
 if [ -z "$FILES" ]; then
