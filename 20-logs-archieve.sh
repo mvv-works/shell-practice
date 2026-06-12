74e43fe724d16bd50861d0b3b77290f47b2ac798
vamsi <vijayvamsi550@gmail.com>
2026-06-12 10:23:42 +0530

    hello

--- 20-logs-archieve.sh
+++ 20-logs-archieve.sh
@@ -26,3 +26,14 @@ if [ -z "$FILES" ]; then
     exit 0
 fi
 
+FILES=$( find "$SOURCE_DIR" -type f -name "*.log" -mtime +$DAYS )
+
+if [ -z "$FILES" ]; then
+    echo "Log files older than 14 days not found, nothing to do"
+    exit 0
+fi
+
+while IFS= read -r FILE
+do 
+    echo "$FILE"
+done <<< "$FILES"
