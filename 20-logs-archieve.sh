2a16388eac19d1364229ec7449bbffa4ecce8488
vamsi <vijayvamsi550@gmail.com>
2026-06-12 11:37:22 +0530

    hello

--- 20-logs-archieve.sh
+++ 20-logs-archieve.sh
@@ -35,4 +35,4 @@ done <<< "$FILES"
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 ARCHIEVE_FILE="$DEST_DIR/logs-archieve-$TIMESTAMP.tar.gz" 
 
-tar -czvf $ARCHIEVE_FILE $FILES  
+tar -czvf $ARCHIEVE_FILE $FILES  &> /dev/null
