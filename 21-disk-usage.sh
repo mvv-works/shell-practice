b49393be6800b886e975853868865f62ca7053d5
vamsi <vijayvamsi550@gmail.com>
2026-06-12 23:26:16 +0530

    hello

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -10,7 +10,7 @@ do
     PARTATION=$( echo $line | awk '{print $7}' )
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="High Disk Usage on $PARTATION: $USAGE <br>"
+        MESSAGE+="High Disk Usage on $PARTATION: $USAGE /n"
     fi
 done <<< "$DISK_USAGE"
 
