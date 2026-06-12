c0f161700ffa6ebc2333618dbcc033172c7a674c
vamsi <vijayvamsi550@gmail.com>
2026-06-12 10:36:00 +0530

    hello

--- 20-logs-archieve.sh
+++ 20-logs-archieve.sh
@@ -4,18 +4,18 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}
 
-if [ -z "SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
+if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
     echo "Either source directory or destination directory empty"
     echo "USAGE:: $0 [source_dir] [dest_dir] [days:default 14]"
     exit 1
 fi
 
-if [ ! -d "SOURCE_DIR" ]; then
+if [ ! -d "$SOURCE_DIR" ]; then
     echo "source directory: $SOURCE_DIR does not exist"
     exit 1
 fi
 
-if [ ! -d "DEST_DIR" ]; then
+if [ ! -d "$DEST_DIR" ]; then
     echo "destination directory: $DEST_DIR does not exist"
     exit 1
 fi
